1b0b11944f5c8e95c6da6718114382022e08efc1
Rowel Maharjan <rowelmhj@gmail.com>
2025-07-09 18:15:49 +0545

    Initial Setup with tailwind

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-400 p-10 bg-green-500 text-3xl">Hello World</div>
+    </>
+  );
+}
+
+export default App;
